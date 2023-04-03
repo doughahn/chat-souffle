d00e46dc012d83f4d4d4827aae5989bd03850a0c
Doug Hahn <doughahn@gmail.com>
2023-04-03 16:03:03 -0700

    Better setup for the LRS switch

--- src/scripts/config/xapi-config.js
+++ src/scripts/config/xapi-config.js
@@ -1,15 +1,30 @@
+// xapi-config.js
+
+// Use $.when() to ensure both libraries are loaded before executing setupXAPIConfig()
+$.when(loadCryptoJS, loadXAPIWrapper).done(function () {
+    setupXAPIConfig();
+});
+
 function setupXAPIConfig() {
-    // test data LRS
-    var devConf = {
+    // Test data LRS
+    const devConf = {
         "endpoint": "https://sample-lrs-befogih.lrs.io/xapi/",
         "auth": "Basic " + CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse("coikpi:rolzak"))
     };
 
-    // prod LRS
-    var conf = {
+    // Production data LRS
+    const prodConf = {
         "endpoint": "https://chat-souffle.lrs.io/xapi/",
         "auth": "Basic " + CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse("gaodun:ujgizo"))
     };
-    
-    ADL.XAPIWrapper.changeConfig(devConf);
+
+    // Set environment to 'development' or 'production'
+    const environment = 'development';
+
+    // Change the configuration based on the environment
+    if (environment === 'development') {
+        ADL.XAPIWrapper.changeConfig(devConf);
+    } else {
+        ADL.XAPIWrapper.changeConfig(prodConf);
+    }
 }
--- src/scripts/helpers/cookie-randomize.js
+++ src/scripts/helpers/cookie-randomize.js
@@ -1,7 +1,7 @@
 // set a cookie to randomize users and assign it to the actor var
 var courseUser = "user" + Math.floor(Math.random() * 89999999 + 10000000);
-if (!("courseRandomUsername" in localStorage)) {
-    localStorage.setItem("courseRandomUsername", courseUser);
+if (!("courseUserName" in localStorage)) {
+    localStorage.setItem("courseUserName", courseUser);
 }
-var actor = localStorage.getItem("courseRandomUsername") || courseUser;
-var actorMbox = "mailto:" + actor + "@yourdomain.com"
+var actor = localStorage.getItem("courseUserName") || courseUser;
+var actorMbox = "mailto:" + actor + "@doughahn.github.io"
